29b64fce7d91d883f3a8fc7ca195627901e990e5
demchenkodenis <denisdemchenko@icloud.com>
2022-02-03 11:04:39 +0300

    Update connect_db.php

--- api/connect_db.php
+++ api/connect_db.php
@@ -1,9 +1,9 @@
 <?php
 
     $host = '127.0.0.1';
-    $db   = 'cf59384_px1f3';
-    $user = 'cf59384_px1f3';
-    $pass = '******';
+    $db   = '***';
+    $user = '***';
+    $pass = '***';
     $charset = 'utf8';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
